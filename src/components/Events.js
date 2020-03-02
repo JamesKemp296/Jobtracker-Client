174bcd3eaa79d0ede6121f71cd8725cae2105085
Pitney <PJules602@gmail.com>
2020-03-02 16:37:55 -0500

    Axios API Call Added - In Progress

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,12 +2,25 @@ import React from 'react'
 import axios from 'axios'
 
 const Events = () => (
+
   <><FetchEvents></FetchEvents></>
 )
 
 const FetchEvents = async e => {
 
   e.PreventDefault()
+
+  axios.get('/https://api.meetup.com/find/groups?zip=33127&radius=1&category=25&order=members', {
+    params: {
+      ID: 12345
+    }
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
 }
 
