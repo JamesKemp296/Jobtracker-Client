f0a90321e7aac2aa00a8aac31a40859790c329cc
Pitney <PJules602@gmail.com>
2020-03-02 16:35:12 -0500

    Events.js Created

---
+++ src/components/Events.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+
+const Events = () => (
+  <><FetchEvents></FetchEvents></>
+)
+
+const FetchEvents = async e => {
+
+  e.PreventDefault()
+
+}
+
+export default Events
