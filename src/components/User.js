9962096f69721abf33858643da10970342bdfd21
Rob Terrell <Rterrell25@gmail.com>
2020-03-03 13:44:57 -0500

    met criteria of assigned issue

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 
